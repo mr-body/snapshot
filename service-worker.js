1e7bf28f36f1f8ef6712ca4344fb45d74483801f
waalexan <walteralexandresantanag@gmail.com>
2024-10-12 22:16:51 -0400

    service worker 2

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,33 @@
-const cacheName = "pwa-conf-v2";
+const cacheName = "pwa-conf-v9";
 const staticAssets = [
-    './',
-    './index.html',
-    './index.js',
-    './manifest.webmanifest',
-    './assets/cat.png',
-    './android-chrome-192x192.png',
-    './android-chrome-512x512.png',
-    './favicon-32x32.png',
-    './favicon-16x16.png',
-    './offline.html',  // Adicione esta linha
+  "./",
+  "./index.html",
+  "./index.js",
+  "./assets/cat.png",
+  "./offline.html",
+  "./service-worker.js"
 ];
 
 self.addEventListener("install", async (event) => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
 });
+
 self.addEventListener("fetch", (event) => {
-    const req = event.request;
-    event.respondWith(cacheFirst(req));
+  const req = event.request;
+  event.respondWith(
+    cacheFirst(req).catch(() => fetchOfflinePage())
+  );
 });
 
 async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cachedResponse = await cache.match(req);
-    return cachedResponse || fetch(req);
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || fetch(req);
+}
+
+async function fetchOfflinePage() {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match("./offline.html");
+  return cachedResponse;
 }
