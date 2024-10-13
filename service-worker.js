899c5bca3ffc0670a7e6a21c2e94e7138f83cfab
waalexan <walteralexandresantanag@gmail.com>
2024-10-12 22:31:05 -0400

    service worker 2

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,25 @@
-const cacheName = "pwa-conf-v9";
+const cacheName = "pwa-conf-v3";
 const staticAssets = [
-  "./",
-  "./index.html",
-  "./index.js",
-  "./assets/cat.png",
-  "./offline.html",
-  "./service-worker.js"
+  "./offline.html"
 ];
 
 self.addEventListener("install", async (event) => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
 });
-
 self.addEventListener("fetch", (event) => {
   const req = event.request;
-  event.respondWith(
-    cacheFirst(req).catch(() => fetchOfflinePage())
-  );
+  event.respondWith(cacheFirst(req));
 });
 
 async function cacheFirst(req) {
   const cache = await caches.open(cacheName);
   const cachedResponse = await cache.match(req);
-  return cachedResponse || fetch(req);
-}
-
-async function fetchOfflinePage() {
-  const cache = await caches.open(cacheName);
-  const cachedResponse = await cache.match("./offline.html");
-  return cachedResponse;
+  if (cachedResponse) return cachedResponse;
+  const networkResponse = await fetch(req);
+  if (!networkResponse.ok) {
+    const offlineResponse = await cache.match("./offline.html");
+    if (offlineResponse) return offlineResponse;
+  }
+  return networkResponse;
 }
