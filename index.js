dad707591376819803850adda7a2556945f76a85
Walter Alexandre Santana <162894298+mr-body@users.noreply.github.com>
2024-10-20 14:13:34 +0000

    tst

--- index.js
+++ index.js
@@ -97,3 +97,27 @@ searchInput.addEventListener("input", () => {
     }
   });
 });
+
+
+
+
+
+
+
+
+
+window.addEventListener("load", (e) => {
+  registerSW();
+});
+
+async function registerSW() {
+  if ("serviceWorker" in navigator) {
+    try {
+      await navigator.serviceWorker.register("./service-worker.js");
+    } catch (e) {
+      alert("ServiceWorker registration failed. Sorry about that.");
+    }
+  } else {
+    document.querySelector(".alert").removeAttribute("hidden");
+  }
+}
